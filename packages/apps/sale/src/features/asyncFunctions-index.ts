58a146e4899ea4f6f86ea51a9ee6250f31cb8039
lme-axelor <102581501+lme-axelor@users.noreply.github.com>
2024-09-18 14:17:31 +0200

    refactor: remove client creation management

--- packages/apps/sale/src/features/asyncFunctions-index.ts
+++ packages/apps/sale/src/features/asyncFunctions-index.ts
@@ -16,8 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export {fetchActiveCart, updateCart} from './cartSlice';
 export {searchCartLine, updateCartLine, deleteCartLine} from './cartLineSlice';
+export {fetchActiveCart, updateCart, validateCart} from './cartSlice';
 export {fetchComplementaryProduct} from './complementaryProductSlice';
 export {fetchCustomerDelivery} from './customerDeliverySlice';
 export {
